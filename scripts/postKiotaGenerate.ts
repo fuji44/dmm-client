2cba9b3a70c0b39633a237e2b4e5635f5f37e32a
Yoshihiro Fujimoto <fuji.44mt@gmail.com>
2024-08-14 09:10:49 +0900

    chore: update post generate script

--- scripts/postKiotaGenerate.ts
+++ scripts/postKiotaGenerate.ts
@@ -1,6 +1,13 @@
-// DenoでdMMClient.tsの文字列を読み込んで、import文の.js拡張子を.ts拡張子に書き換える。
+// Kiota generate code modification
+
 const dMMClient = await Deno.readTextFile("client/dMMClient.ts");
-const replaced = dMMClient.replace(/\.js/g, ".ts");
+
+const replaced = dMMClient
+  // replace .js to .ts
+  .replace(/\.js/g, ".ts")
+  // append return type
+  .replace(/(function createDMMClient.*\)) \{/g, "$1: DMMClient {");
+// add return
 await Deno.writeTextFile("client/dMMClient.ts", replaced);
 
 console.log("Done replaced .js to .ts in dMMClient.ts");
